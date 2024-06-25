a067465468139bed85850625d5733db75573b73b
pinkypops <pinkypops@protonmail.com>
2024-06-25 01:22:27 +0100

    demonstrate that the pointer itself can be changed

--- constants_in_C++.cpp
+++ constants_in_C++.cpp
@@ -3,10 +3,13 @@
 int main()
 {
 	int a = 42; 
-	const int * pa = &a;	// pA is a pointer to a constant int.
+	const int * pa = &a;			// pA is a pointer to a constant int.
 	std::cout << "Before: " << *pa << std::endl;
 	//*pa = 5;						// error! trying to modify via a ptr to readonly.
-	a = 41;								// assigning to *pa fails, but assing to a is fine.   
+	a = 41;						 	// assigning to *pa fails, but assigning to a is fine.   
+	std::cout << "pa = : " << std::hex << pa << std::endl;
+	pa++;							// pa is a ptr to const, but not const itself
+	std::cout << "pa = : " << std::hex << pa << std::endl;
 	std::cout << "After: " << *pa << std::endl;
 	return 0;
 }
